0dfd03fde83f5d2a7349cc860fa6e4e99e3b5005
Marie <mcari1311@gmail.com>
2022-07-12 00:00:32 -0400

    hardest level

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function checkWin(board, player) {
 function gameOver(gameWon) {
 	for (let index of winCombos[gameWon.index]) {
 		document.getElementById(index).style.backgroundColor =
-			gameWon.player == huPlayer ? "brown" : "lightpink";
+        gameWon.player == huPlayer ? "brown" : "lightpink";
 	}
 	for (let i = 0; i < cells.length; i++) {
 		cells[i].removeEventListener('click', turnClick, false);
